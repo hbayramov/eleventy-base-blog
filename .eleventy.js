6bc9f1d49d1bb6e288b02795d4cfab561cce2aad
Huseyn Bayramov <hsyn.bayramov@gmail.com>
2022-02-18 22:16:37 +0400

    netlify cms config

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ const markdownItAnchor = require("markdown-it-anchor");
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("css");
 
   // Add plugins
